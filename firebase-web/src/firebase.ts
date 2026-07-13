0cce74f1de027cd7e29fba6d0ce13521c89696e9
Numore99 <lalo.giraudo@gmail.com>
2026-07-12 22:06:34 -0300

    Fix Firebase web authentication configuration

--- firebase-web/src/firebase.ts
+++ firebase-web/src/firebase.ts
@@ -1,5 +1,5 @@
 import {initializeApp} from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
-const app=initializeApp({apiKey:"AIzaSyBYEm8V0ffiJPuy3spPXc-quXD0czHWu2k",authDomain:"shopcerto.firebaseapp.com",projectId:"shopcerto",appId:"1:362976893415:web:4cb5e39e8a8629c5bd0542"});
+const app=initializeApp({apiKey:"AIzaSyBYEm8VOffiTPuy3spPXc-quXDOczHWu2k",authDomain:"shopcerto.firebaseapp.com",projectId:"shopcerto",appId:"1:362976893415:web:4cb5e39e8a8629c5bd0542"});
 export const auth=getAuth(app);export const db=getFirestore(app);export const googleProvider=new GoogleAuthProvider();
